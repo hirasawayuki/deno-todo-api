3388c5e2ad02b200aad3d41c77ec935b89ac10a7
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-07 22:24:52 +0900

    add router

---
+++ router.ts
@@ -0,0 +1,7 @@
+import { Router } from "./deps.ts";
+import { rootHandler, todosHandler } from "./middlewares/mod.ts";
+
+export const router = new Router();
+router.get("/", rootHandler.getHome);
+router.get("/todo", todosHandler.getTodo);
+
