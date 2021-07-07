66510b808bba8cfa901b024308a78fb290e8fe4f
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-07 22:24:08 +0900

    add server

---
+++ server.ts
@@ -0,0 +1,18 @@
+import { Application, bold, yellow } from "./deps.ts";
+import { router } from "./router.ts";
+import { logger, errorHandler } from "./middlewares/mod.ts";
+
+const app = new Application();
+
+app.use(logger);
+app.use(errorHandler);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({ hostname, port }) => {
+  console.log(bold(`Start listening on ${hostname || "localhost"}`)+yellow(`:${port}`));
+})
+
+const port = parseInt(Deno.env.get("PORT") || "8000");
+
+app.listen({port: port});
