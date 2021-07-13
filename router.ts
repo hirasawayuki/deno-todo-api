df79f1f17b76468cf761a5fda806b84935438c00
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-13 21:50:27 +0900

    add user register endpoint

--- router.ts
+++ router.ts
@@ -1,5 +1,7 @@
 import { Router } from "./deps.ts";
 import { rootHandler, todosHandler } from "./handler/mod.ts";
+import { Register } from "./controllers/auth.controller.ts";
+import { RegisterValidation } from "./validations/register.validation.ts";
 
 export const router = new Router();
 
@@ -9,3 +11,4 @@ router.get("/todos/:id", todosHandler.get);
 router.post("/todos", todosHandler.create);
 router.put("/todos/:id", todosHandler.update);
 router.delete("/todos/:id", todosHandler.remove);
+router.post('/api/register',  RegisterValidation, Register);
