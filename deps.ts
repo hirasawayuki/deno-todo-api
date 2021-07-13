0f6bc56be8c53b73acba96c74a0019a493a3377a
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-13 21:49:38 +0900

    add validasaur package

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
 export { Application, Router, Status, Context, isHttpError } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 export { bold, yellow, green, cyan } from "https://deno.land/std@0.100.0/fmt/colors.ts";
 export { v4 as uuid } from "https://deno.land/std@0.54.0/uuid/mod.ts";
+export { validate, required, isEmail } from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
+
 export type { RouterContext } from "https://deno.land/x/oak@v7.7.0/mod.ts";
