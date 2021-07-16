e2f99390864609450c1ecac9adcfa0a0371c3709
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-16 23:40:46 +0900

    fix server.ts

--- server.ts
+++ server.ts
@@ -1,12 +1,13 @@
 import { Application, bold, yellow } from "./deps.ts";
 import { router } from "./routers/router.ts";
-import { logger, errorHandler } from "./middlewares/mod.ts";
+import { logger, errorHandler, notFoundHandler } from "./middlewares/mod.ts";
 
 const app = new Application();
 app.use(logger);
 app.use(errorHandler);
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(notFoundHandler);
 
 app.addEventListener("listen", ({ hostname, port }) => {
   console.log(bold(`Start listening on ${hostname || "localhost"}`)+yellow(`:${port}`));
