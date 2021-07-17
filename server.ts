852b255248d208a4075b8ce036a6651197176c8a
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-17 22:08:00 +0900

    Fix server

--- server.ts
+++ server.ts
@@ -1,18 +1,21 @@
 import { Application, bold, yellow } from "./deps.ts";
 import { router } from "./routers/router.ts";
-import { logger, errorHandler, notFoundHandler } from "./middlewares/mod.ts";
+import { logger, internalServerErrorHandler, notFoundErrorHandler } from "./middlewares/mod.ts";
 
-const app = new Application();
-app.use(logger);
-app.use(errorHandler);
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.use(notFoundHandler);
+const listenAndServe = () => {
+  const app = new Application();
+  app.use(logger);
+  app.use(internalServerErrorHandler);
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+  app.use(notFoundErrorHandler);
 
-app.addEventListener("listen", ({ hostname, port }) => {
-  console.log(bold(`Start listening on ${hostname || "localhost"}`)+yellow(`:${port}`));
-})
+  app.addEventListener("listen", ({ hostname, port }) => {
+    console.log(bold(`Start listening on ${hostname || "localhost"}`)+yellow(`:${port}`));
+  })
 
-const port = parseInt(Deno.env.get("PORT") || "8000");
+  const port = parseInt(Deno.env.get("PORT") || "8000");
+  app.listen({port: port});
+}
 
-app.listen({port: port});
+listenAndServe();
