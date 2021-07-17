d8d6af36b9c62787332f5190f2c60786034e3876
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-17 22:06:13 +0900

    Fix deps file

--- deps.ts
+++ deps.ts
@@ -4,6 +4,5 @@ export { v4 as uuid } from "https://deno.land/std@0.54.0/uuid/mod.ts";
 export { validate, required, isEmail } from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
 export { create, getNumericDate, verify } from "https://deno.land/x/djwt@v2.2/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
-
 export type { RouterContext } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 export type { Payload } from "https://deno.land/x/djwt@v2.2/mod.ts";
