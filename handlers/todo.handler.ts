d5872af462bc46548bd8f328963be469a4e904c7
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-19 23:37:17 +0900

    Add TodoHandler test case

--- handlers/todo.handler.ts
+++ handlers/todo.handler.ts
@@ -3,7 +3,7 @@ import { getParams } from "../middlewares/utils.ts";
 import { TodoRepository } from "../repositories/todo.repository.ts";
 
 interface JwtService {
-  userId(jwt: string): Promise<string>;
+  userId(ctx: RouterContext): Promise<string>;
 }
 
 export class TodoHandler {
@@ -14,7 +14,7 @@ export class TodoHandler {
   }
 
   async getAll(ctx: RouterContext): Promise<void> {
-    const userId = "e161f4eb-8cbe-404f-9d47-3651f2bafe9a";
+    const userId = await this.jwtService.userId(ctx);
     const todos = await this.todoRepository.findByUserId(userId);
     ctx.response.status = Status.OK;
     ctx.response.body = {
@@ -23,8 +23,8 @@ export class TodoHandler {
   }
 
   async get(ctx: RouterContext): Promise<void> {
-    const { id } = await getParams(ctx);
-    const [todo, error] = await this.todoRepository.find(id);
+    const { id } = ctx.params;
+    const [todo, error] = await this.todoRepository.find(id || "");
     if (error) {
       ctx.response.status = Status.BadRequest;
       ctx.response.body = {
@@ -40,7 +40,7 @@ export class TodoHandler {
 
   async create(ctx: RouterContext): Promise<void> {
     const params = await getParams(ctx);
-    const userId = await this.jwtService.userId(ctx.cookies.get("jwt") || "");
+    const userId = await this.jwtService.userId(ctx);
     await this.todoRepository.create(params.title, userId);
     ctx.response.status = Status.OK;
     ctx.response.body = {
--- service/jwt.service.ts
+++ service/jwt.service.ts
@@ -1,4 +1,4 @@
-import { create, getNumericDate, Payload, verify } from "../deps.ts";
+import { RouterContext, create, getNumericDate, Payload, verify } from "../deps.ts";
 
 export class JwtService {
   async create(id: string): Promise<string> {
@@ -22,9 +22,9 @@ export class JwtService {
     }
   }
 
-  async userId(jwt: string): Promise<string> {
+  async userId(ctx: RouterContext): Promise<string> {
     const key = Deno.env.get("SECRET_KEY") || "";
-
+    const jwt = ctx.cookies.get("jwt") || "";
     try {
       const { id } = await verify(jwt, key, "HS512");
       return id as string;
