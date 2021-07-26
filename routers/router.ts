8db230bc01d0d65c8337ecdcd1828b6043572ea7
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-07-26 23:32:35 +0900

    Renamed from jwt service to jwt util

--- routers/router.ts
+++ routers/router.ts
@@ -7,7 +7,7 @@ import {
 } from "../handlers/mod.ts";
 import { authMiddleware } from "../middlewares/mod.ts";
 import { TodoRepository, UserRepository } from "../repositories/mod.ts";
-import { JwtService } from "../service/mod.ts";
+import { Jwt } from "../utils/mod.ts";
 import { loginValidation, registerValidation } from "../validations/mod.ts";
 
 export const router = new Router();
@@ -17,7 +17,7 @@ const rootHandler = new RootHandler();
 router.get("/v1", (ctx) => rootHandler.getHome(ctx));
 
 // Authenticate
-const authHandler = new AuthHandler(new UserRepository(), new JwtService());
+const authHandler = new AuthHandler(new UserRepository(), new Jwt());
 router.post(
   "/v1/signup",
   registerValidation.RegisterValidation,
@@ -31,11 +31,11 @@ router.post(
 router.post("/v1/logout", (ctx) => authHandler.logout(ctx));
 
 // User
-const userHandler = new UserHandler(new UserRepository(), new JwtService());
+const userHandler = new UserHandler(new UserRepository(), new Jwt());
 router.get("/v1/user", authMiddleware, (ctx) => userHandler.getUser(ctx));
 
 // Todos
-const todoHandler = new TodoHandler(new TodoRepository(), new JwtService());
+const todoHandler = new TodoHandler(new TodoRepository(), new Jwt());
 router.get("/v1/todos", authMiddleware, (ctx) => todoHandler.getAll(ctx));
 router.get("/v1/todos/:id", authMiddleware, (ctx) => todoHandler.get(ctx));
 router.post("/v1/todos", authMiddleware, (ctx) => todoHandler.create(ctx));
